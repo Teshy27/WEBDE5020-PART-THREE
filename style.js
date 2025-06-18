255e142a42fc5879679d59a6fc3d10a3d352e969
Teshy27 <lithembahela@gmail.com>
2025-06-18 13:24:42 +0000

    .

--- style.js
+++ style.js
@@ -1,7 +1,6 @@
-window.onload=function() {
-    var applicantName !=null && applicantName !=""){
-        alert("Welcome,,to Infinity Gold"+ aplicantName);
-    } else{
-        alert"(Oh No! seems like you're too young to be accomodated");
-    }
-}
+// Pop-up greeting using alert
+alert(""Hello,{name},Welcome to the Infinity Gold.");
+// Optionally reset the form
+form.reset();
+nameInput.focus();
+});
